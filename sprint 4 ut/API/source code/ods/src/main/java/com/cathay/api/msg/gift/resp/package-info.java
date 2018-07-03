6e8bc2d199408d1511dc722ab1cb3b5e1f11c2d0
timothy-fidelis-pw <timothy.fidelis@pointwest.com.ph>
2018-07-03 19:21:53 +0800

    zip the source code file, ods

--- sprint 4 ut/API/source code/ods/src/main/java/com/cathay/api/msg/gift/resp/package-info.java
+++
@@ -1,7 +0,0 @@
-
-@XmlSchema(
-		namespace="http://www.cathaybk.com.tw/webservice/mis99",
-		elementFormDefault=javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.cathay.api.msg.gift.resp;
-
-import javax.xml.bind.annotation.XmlSchema;
