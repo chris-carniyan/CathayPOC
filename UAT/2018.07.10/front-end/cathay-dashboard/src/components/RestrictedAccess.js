014022fb6f86ce7552753cb3250072451b3cba40
timothy-fidelis-pw <timothy.fidelis@pointwest.com.ph>
2018-07-10 21:07:30 +0800

    Zipped the dashboard folder

--- UAT/2018.07.10/front-end/cathay-dashboard/src/components/RestrictedAccess.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const restrictedAccessPage = ({ message }) => (
-  <div className="retrictedAccessContainer">
-    <div className="retrictedAccessMessage">{message}</div>
-  </div>
-);
-
-export default restrictedAccessPage;
